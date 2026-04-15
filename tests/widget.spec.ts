e7c8bcb320929a84c909c3cf4570ed6912aceed2
Garu2807 <arutyunyangagik@gmail.com>
2026-04-15 23:55:03 +0300

    test fix

--- tests/widget.spec.ts
+++ tests/widget.spec.ts
@@ -1,34 +1,34 @@
-import { test, expect } from '@playwright/test';
-import {WidgetPage} from "./widget.page";
+import { test, expect } from "@playwright/test";
+import { WidgetPage } from "./widget.page";
 
-test.describe('Uchi.ru widget ', () => {
+test.describe("Uchi.ru widget ", () => {
   let widgetPage: WidgetPage;
 
-  test.beforeEach(async ({page}) => {
+  test.beforeEach(async ({ page }) => {
     widgetPage = new WidgetPage(page);
 
     // open uchi.ru main page
-    await page.goto('/');
+    await page.goto("/");
 
     // close cookies popup
-    await page.click('._UCHI_COOKIE__button');
+    await page.click("._UCHI_COOKIE__button");
   });
 
-  test('opens', async ({page}) => {
+  test("opens", async ({ page }) => {
     await widgetPage.openWidget();
 
-    await expect(widgetPage.getWidgetBody()).toBeVisible()
+    await expect(widgetPage.getWidgetBody()).toBeVisible();
   });
 
-  test('has correct title', async ({ page }) => {
+  test("has correct title", async ({ page }) => {
     await widgetPage.openWidget();
 
     const articles = await widgetPage.getPopularArticles();
 
-    await articles[0].click();
+    await articles.nth(0).click();
 
     await widgetPage.clickWriteToUs();
 
-    expect(await widgetPage.getTitle()).toEqual('Связь с поддержкой');
+    expect(await widgetPage.getTitle()).toEqual("Связь с поддержкой");
   });
 });
